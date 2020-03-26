f4238b160ad75401a4defbc818ac4d534637aaf2
Jos Craw <jos@joscraw.net>
2020-03-26 19:27:59 +1300

    Fixed to work with docker

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,4 @@ RUN pip install --user mcrcon discord.py
 WORKDIR /work
 COPY bot.py .
 
-ENV DISCORD_TOKEN=NjkxODEzNTc3MzM3NzMzMTUx.XnlcFg.oIWLv6nIHjz_VazpeD7RxcLZx0Q
-ENV DISCORD_GUILD=Ciiiv
-ENV MC_SERVER_IP=192.168.1.99
-ENV MC_RCON_PORT=25575
-ENV MC_RCON_PASS=supersecretpasscodething
-
 CMD python bot.py
--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import os
-import socket
 import asyncio
 from mcrcon import MCRcon
 from discord.ext import commands
+# from dotenv import load_dotenv
+# load_dotenv()
 import time
 
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -11,16 +12,14 @@ MC_SERVER_IP = os.getenv('MC_SERVER_IP')
 MC_RCON_PORT = os.getenv('MC_RCON_PORT')
 MC_RCON_PASS = os.getenv('MC_RCON_PASS')
 
-
 bot = commands.Bot(command_prefix='!')
 difficulty_array = ['easy', 'normal', 'hard']
 
 proposals = list()
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((MC_SERVER_IP, int(MC_RCON_PORT)))
-
-mcrcon = MCRcon(MC_SERVER_IP, MC_RCON_PASS)
+mcrcon = MCRcon(str(MC_SERVER_IP), str(MC_RCON_PASS))
+mcrcon.connect()
+print(mcrcon)
 if not mcrcon:
     print('Password incorrect')
     exit(1)
@@ -103,67 +102,64 @@ class DifficultyProposal(Proposal):
 
 def announce(message):
     command = "say {}".format(message)
-    mcrcon.command(sock, command)
+    mcrcon.command(command)
 
 def check_difficulty():
-    difficulty = mcrcon.command(sock, 'difficulty')
+    difficulty = mcrcon.command('difficulty')
     print(difficulty)
     difficulty = difficulty.split(' ')
     return difficulty[3].lower()
 
 def change_difficulty(difficulty):
     print('Changing difficulty')
-    mcrcon.command(sock, "difficulty {}".format(difficulty))
+    mcrcon.command("difficulty {}".format(difficulty))
 
 @bot.command(name='info', help='Lists all pending proposals')
 async def info(ctx):
-    if GUILD == str(ctx.guild) or GUILD == 'Test Minecraft Server':
-        await ctx.send("Active Proposals: {}".format(', \n'.join(str(proposal) for proposal in proposals)))
+    await ctx.send("Active Proposals: {}".format(', \n'.join(str(proposal) for proposal in proposals)))
 
 @bot.command(name='vote', help='Casts a vote for a specified proposal')
 async def vote(ctx, proposal_id: int, vote: str):
     print("id: {}, vote: {}".format(proposal_id, vote))
-    if GUILD == str(ctx.guild) or GUILD == 'Test Minecraft Server':
-        for proposal in proposals:
-            if proposal_id == proposal.id:
-                print('found proposal')
-                if vote.lower() == 'yes':
-                    print('yes')
-                    proposal.vote_for(ctx.author)
-                    await ctx.send(proposal.get_voters())
-                elif vote.lower() == 'no':
-                    print('no')
-                    proposal.vote_against(ctx.author)
-                    await ctx.send(proposal.get_voters())
-                else:
-                    await ctx.send("Vote must either be 'yes' or 'no'")
+    for proposal in proposals:
+        if proposal_id == proposal.id:
+            print('found proposal')
+            if vote.lower() == 'yes':
+                print('yes')
+                proposal.vote_for(ctx.author)
+                await ctx.send(proposal.get_voters())
+            elif vote.lower() == 'no':
+                print('no')
+                proposal.vote_against(ctx.author)
+                await ctx.send(proposal.get_voters())
+            else:
+                await ctx.send("Vote must either be 'yes' or 'no'")
 
 @bot.command(name='propose', help='Create a new proposal to vote on')
 async def propose_difficulty(ctx, proposal_type: str, arg: str):
-    if GUILD == str(ctx.guild) or GUILD == 'Test Minecraft Server':
-        if proposal_type == 'difficulty':
-            if difficulty_array.__contains__(arg):
-                if check_difficulty() != arg:
-                    proposal = DifficultyProposal(ctx, arg)
-                    proposals.append(proposal)
-                    await ctx.send(str(proposal))
-                    await proposal.tick()
-                else:
-                    await ctx.send("The server is already at that difficulty")
+    if proposal_type == 'difficulty':
+        if difficulty_array.__contains__(arg):
+            if check_difficulty() != arg:
+                proposal = DifficultyProposal(ctx, arg)
+                proposals.append(proposal)
+                await ctx.send(str(proposal))
+                await proposal.tick()
             else:
-                await ctx.send("Not a vaild difficulty :(")
+                await ctx.send("The server is already at that difficulty")
+        else:
+            await ctx.send("Not a vaild difficulty :(")
 
 @bot.command(name='command', help='Runs command on server')
 @commands.has_role('server_admin')
 async def command(ctx, *args):
     command = " ".join(args[:])
-    resp = mcrcon.command(sock, command)
+    resp = mcrcon.command(command)
     await ctx.send(resp)
 
 @bot.command(name='zzz', help='Sets time to day if night')
 @commands.has_role('server_admin')
 async def zzz(ctx):
-    time_data = mcrcon.command(sock, 'time')
+    time_data = mcrcon.command('time')
     time_data = time_data.split('\n')
     time_data = time_data[0]
     time_data = time_data.split()
@@ -174,7 +170,7 @@ async def zzz(ctx):
     time_data = int(time_data[0])*100 + int(time_data[1])
     if time_data < 600 or time_data > 2000:
         print('Setting time to day')
-        mcrcon.command(sock, 'time set day')
+        mcrcon.command('time set day')
         await ctx.send("Wakey Wakey!")
     else:
         await ctx.send("It isn't night time yet, the time is: {}".format(current_time))
@@ -182,8 +178,9 @@ async def zzz(ctx):
 @bot.command(name='share', help='Share the message with anyone on the server')
 async def share(ctx, *args):
     message = " ".join(args[:])
+    print(message)
     command = 'say <{}>: {}'.format(ctx.author.name, message)
-    mcrcon.command(sock, command)
+    mcrcon.command(command)
 
 
 @bot.event
